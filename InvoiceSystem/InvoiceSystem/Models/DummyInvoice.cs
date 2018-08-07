c90fcfd4646da3d8471a9b1aa5b4196f5aa13d3a
amrayman100 <amrayman1996@yahoo.com>
2018-08-07 13:09:40 -0700

    .

---
+++ InvoiceSystem/InvoiceSystem/Models/DummyInvoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceSystem.Models
+{
+    public class DummyInvoice
+    {
+        public int InvoiceNo { get; set; }
+        public String IssueDate { get; set; }
+        public String CollectionDate { get; set; }
+        public String ActualCollDate { get; set; }
+        public String Customer { get; set; }
+        public float Amount { get; set; }
+        public bool Collected { get; set; }
+        public bool Suspended { get; set; }
+
+
+        public DummyInvoice()
+        {
+            Collected = false;
+            Suspended = false;
+        }
+
+
+
+    }
+}
---
+++ InvoiceSystem/InvoiceSystem/ViewModel/InvoiceViewModel.cs
@@ -0,0 +1,13 @@
+﻿using InvoiceSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceSystem.ViewModel
+{
+    public class InvoiceViewModel
+    {
+        public List<DummyInvoice> Invoices { get; set; }
+    }
+}
