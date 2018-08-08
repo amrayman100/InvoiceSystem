3a4053162785358b3c20b05a511db44907dec1bd
amrayman100 <amrayman1996@yahoo.com>
2018-08-08 05:20:18 -0700

    Customer Api

--- InvoiceSystem/Collection.APIs/CustomerApi.cs
+++ InvoiceSystem/Collection.APIs/CustomerApi.cs
@@ -5,10 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
-
+using Collection.DSL;
 namespace Collection.APIs
 {
     public class CustomerApi : ApiController
     {
+        CustomerDSL c = new CustomerDSL();
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetAllCustomers()
+        {
+
+            return Ok(c.GetCustomers());
+        }
+
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult addCustomer()
+        {
+            if (!ModelState.IsValid)
+            {
+
+            }
+            return Ok();
+        }
+
     }
 }
