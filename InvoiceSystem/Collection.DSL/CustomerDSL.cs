251794b17a8f1e9a198ed18277663cbf4ad1be2a
amira atef ahmed <amiraatef_96@outlook.com>
2018-08-08 13:16:37 +0200

    repo. and DSL customer

---
+++ InvoiceSystem/Collection.DSL/CustomerDSL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Collection.Repository;
+using Collection.DAL;
+
+namespace Collection.DSL
+{
+   
+    public class CustomerDSL
+    {
+        CustomerRepo repo = new CustomerRepo();
+        public IEnumerable<Customer> GetCustomers()
+        {
+            var list = repo.GetCustomers();
+            return list;
+        }
+        public Customer GetCustomerByID(int id)
+        {
+            var customer = repo.GetCustomerByID(id);
+            return customer;
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+            repo.InsertCustomer(customer);
+        }
+        public void Delete(int id)
+        {
+            repo.DeleteCustomer(id);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            repo.UpdateCustomer(customer);
+        }
+
+        public void CommitCustomers()
+        {
+            repo.Commit();
+        }
+    }
+}
---
+++ InvoiceSystem/Collection.Repository/CustomerRepo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Collection.DAL;
+//using InvoiceSystem.Models;
+using System.Data.Entity;
+
+namespace Collection.Repository
+{
+    public class CustomerRepo
+    {
+        private invoicesystemEntities context;
+        public CustomerRepo()
+        {
+            context = new invoicesystemEntities();
+        }
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return context.Customers.ToList();
+        }
+        public Customer GetCustomerByID(int id)
+        {
+            return context.Customers.Find(id);
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+            context.Customers.Add(customer);
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            Customer customer = context.Customers.Find(id);
+            context.Customers.Remove(customer);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            context.Entry(customer).State = EntityState.Modified;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+    }
+}
