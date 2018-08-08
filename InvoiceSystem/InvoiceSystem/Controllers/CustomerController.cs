87059c047ccb836cce57c4d34f1d30b10c3c5eb2
amira atef ahmed <amiraatef_96@outlook.com>
2018-08-08 16:59:52 +0200

    customer controller

---
+++ InvoiceSystem/InvoiceSystem/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Collection.DSL;
+using Collection.DAL;
+
+namespace InvoiceSystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+
+        CustomerDSL cust = new CustomerDSL();
+        public ActionResult Index()
+        {
+           
+            return View(cust.GetCustomers());
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Customer c)
+        {
+
+            if (c != null)
+            {
+                cust.InsertCustomer(c);
+            }
+            return View();
+        }
+    }
+}
