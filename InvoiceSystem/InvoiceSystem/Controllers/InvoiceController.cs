9b077e56b381ea63ebbc7ec39c548077d6a3a833
amira atef ahmed <amiraatef_96@outlook.com>
2018-08-09 14:00:39 +0200

    ..

--- InvoiceSystem/InvoiceSystem/Controllers/InvoiceController.cs
+++ InvoiceSystem/InvoiceSystem/Controllers/InvoiceController.cs
@@ -14,7 +14,6 @@ namespace InvoiceSystem.Controllers
         // GET: Invoice
         InvoiceDSL i = new InvoiceDSL();
         public ActionResult Index()
-
         {
 
             /*Customer c1 = new Customer { Cust_No = 1, Name = "el borg" };
@@ -25,13 +24,43 @@ namespace InvoiceSystem.Controllers
                new Invoice{ID = 2 , Cust_ID = 2 , Act_CollectedDate = new DateTime(2017,2,8), Amount = 2000, Collected = true , Invoice_No = 13 , Suspended = false , CollectDate = new DateTime(2017,2,8), IssueDate = new DateTime(2017,2,8), Customer = c2},
 
             };*/
-            var invoices = i.GetInvoices();
-            InvoiceViewModel v = new InvoiceViewModel
+
+            return View(i.GetInvoices());
+        }
+
+        public ActionResult Search()
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Invoice invoice)
+        {
+
+            if (invoice != null)
             {
-                Invoices = invoices
-            };
-            return View(v);
-            
+                i.InsertInvoice(invoice);
+                i.CommitInvoices();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            if (id > 0)
+            {
+                i.Delete(id);
+                i.CommitInvoices();
+            }
+            return RedirectToAction("Index");
         }
     }
+        
 }
