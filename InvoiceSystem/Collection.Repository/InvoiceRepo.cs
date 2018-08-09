af8a4d578cc9a37cac66a79363a48f35e240e88b
amira atef ahmed <amiraatef_96@outlook.com>
2018-08-09 11:53:38 +0200

    ..

--- InvoiceSystem/Collection.Repository/InvoiceRepo.cs
+++ InvoiceSystem/Collection.Repository/InvoiceRepo.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Collection.DAL;
+using System.Data.Entity;
 
 namespace Collection.Repository
 {
-    class InvoiceRepo
+    public class InvoiceRepo
     {
+        private invoicesystemEntities context;
+        public InvoiceRepo()
+        {
+            context = new invoicesystemEntities();
+        }
+        public IEnumerable<Invoice> GetInvoices()
+        {
+            return context.Invoices.ToList();
+        }
+        public Invoice GetInvoiceByID(int id)
+        {
+            return context.Invoices.Find(id);
+        }
+
+        public void InsertInvoice(Invoice invoice)
+        {
+            context.Invoices.Add(invoice);
+
+        }
+
+        public void DeleteInvoice(int id)
+        {
+            Invoice invo = context.Invoices.Find(id);
+            context.Invoices.Remove(invo);
+
+        }
+
+        public void UpdateInvoice(Invoice invo)
+        {
+            context.Entry(invo).State = EntityState.Modified;
+
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
     }
 }
